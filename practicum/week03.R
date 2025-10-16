384d5adedc2d83f0f99894150135b4b1682f9a03
iliana-miladinova <iliana.miladinova@abv.bg>
2025-10-16 11:48:52 +0300

    Add task

---
+++ practicum/week03.R
@@ -0,0 +1,18 @@
+#task18
+#a
+dbinom(2,10,1/6)
+#b
+rbinom(2,10,1/6)
+#c
+1 - rbinom(1,10,1/6)
+#d
+rbinom(8,10,1/6) - rbinom(2,10,1/6)
+
+#task19
+#a
+rgeom(10, 1/6)
+#b
+1 - rgeom(5, 1/6)
+
+#task20
+rnbinom(20-3, 3, 1/6)
